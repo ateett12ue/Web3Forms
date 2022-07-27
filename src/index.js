ce76cb25d4dbd052d5c8a7ea29a685b706c13f87
ateet <ateet@propelld.com>
2022-07-28 02:11:19 +0530

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+// import { ConfigProvider } from "antd";
+// import enUS from "antd/lib/locale-provider/en_US";
+import { ChakraProvider } from '@chakra-ui/react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MoralisProvider appId="xxxxx" serverUrl="xxxxx">
+      <NotificationProvider>
+        {/* <ConfigProvider locale={enUS}> */}
+        {/* <ChakraProvider> */}
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        {/* </ChakraProvider> */}
+        {/* </ConfigProvider> */}
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Box bg='tomato' w='100%' p={4} color='white'>
+        This is the Box
+    </Box>
+  )
+}
+
+export default Home
