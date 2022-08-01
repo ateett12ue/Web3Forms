ee77385d610421b77cc3fe01d052b2c0b357d33f
ateet <ateet@propelld.com>
2022-08-02 02:01:46 +0530

    added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
-
+import { Box, Text } from '@chakra-ui/react'
+import {ConnectButton} from "web3uikit"
 const Home = () => {
+    
   return (
-    <Box bg='tomato' w='100%' p={4} color='white'>
-        This is the Box
+    <Box>
+        <Box bg='tomato' w='100%' py={13} color='white' display={'flex'} alignItems="flex-start" justifyContent="space-evenly">
+            <Box>
+                <Text fontSize={24} fontFamily={"cursive"}>
+                    3FORMS
+                </Text>
+            </Box>
+            <Box>
+                <Text fontSize={21} fontFamily={"Goudy Bookletter 1911"}>
+                    Options
+                </Text>
+            </Box>
+            <Box>
+                <ConnectButton/>
+            </Box>
+        </Box>
+        <Box bg='tomato' w='100%' height='676px' color='white' display={'flex'} alignItems="flex-start" justifyContent="space-evenly">
+            <Box>
+                <Text fontSize={24} fontFamily={"cursive"}>
+                    3FORMS
+                </Text>
+            </Box>
+            <Box>
+                <Text fontSize={21} fontFamily={"Goudy Bookletter 1911"}>
+                    Images
+                </Text>
+            </Box>
+        </Box>
     </Box>
   )
 }
