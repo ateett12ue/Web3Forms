ca145fc765fa7130d6bbdeb83a15e42e93777dcc
ateet <ateet@propelld.com>
2022-08-05 02:29:41 +0530

    create form modal updated

--- src/pages/CreateFormModal.js
+++ src/pages/CreateFormModal.js
@@ -1,32 +1,62 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import {Box,Image, useDisclosure} from "@chakra-ui/react"
 import{
 Modal,
 ModalOverlay,
 ModalContent,
 ModalHeader,
-ModalFooter,
 ModalBody,
 ModalCloseButton,
 } from '@chakra-ui/react'
+import FormsCard from "./FormsCard";
 const CreateFormModal = (props) => {
+  const formTemplates = [
+    {
+      title: 'General',
+      description: 'Click Here To Create General Form',
+      toolTip: 'General Forms are just simple forms with sign capabilities',
+      logo:'servers',
+      link: '/create-form/general-template'
+    },
+    {
+      title: 'Survey',
+      description: 'Click Here To Create Survey Form',
+      toolTip: 'Survey Forms needs you to pool ERC20 tokens, which will be paid to form users',
+      logo:'chest',
+      link: '/create-form/survey-template'
+    },
+    {
+      title: 'Collection',
+      description: 'Click Here To Create Collection Form',
+      toolTip: 'Collection Forms let you get paid by your form user',
+      logo:'token',
+      link: '/create-form/collection-template'
+    }
+  ]
+  const closeModal = (formType) => {
+    props.closeModal()
+  }
   return (
-    <Modal isOpen={props.openModal}>
+    <Modal motionPreset="slideInBottom" isOpen={props.openModal} onClose={props.closeModal} size="6xl">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create a new 3Form</ModalHeader>
+          <ModalHeader>Create a new form</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Box display={"flex"} justifyContent="space-evenly" alignItems={"center"}>
-                <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                    {/* <Image src={"https://bit.ly/2Z4KKcF"} alt={"property.imageAlt"} /> */}
-                </Box>
-                <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                    <Image src={"https://bit.ly/2Z4KKcF"} alt={"property.imageAlt"} />
-                </Box>
-                <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                    {/* <Image src={"https://bit.ly/2Z4KKcF"} alt={"property.imageAlt"} /> */}
-                </Box>
+            <Box display={"flex"} justifyContent="space-evenly" alignItems={"center"} padding={15} marginBottom={18}>
+              {
+                formTemplates.map((temp)=>{
+                  return(
+                      <Box key={temp.title} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                        <FormsCard data={temp} onClick={()=>closeModal()}/>
+                      </Box>
+                  )
+                })
+              }
+              {/* <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                <FormsCard data={temp[0]}/>
+              </Box> */}
             </Box>
           </ModalBody>
         </ModalContent>
