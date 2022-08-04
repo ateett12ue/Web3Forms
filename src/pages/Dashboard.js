afa8ec84d9498263ef086b5fef469a302f87b178
ateet <ateet@propelld.com>
2022-08-05 02:30:05 +0530

    dashboard completed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,62 +1,71 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import CreateFormModal from "./CreateFormModal"
-import {Box, Text} from "@chakra-ui/react"
-import { Routes, Route } from 'react-router-dom'
-import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
-const Dashboard = () => {
-  return (
-    <Box>
-        <CreateFormModal openModal={false}/>
-        <Box h="500px" w="100%" bgColor={"red"} display="flex" justifyContent="space-evenly" alignItems={"flex-start"}>
-            <TableContainer>
-                <Table variant='simple' w={'1000px'}>
-                    <Thead bg={"green"}>
-                    <Tr>
-                        <Th>Name</Th>
-                        <Th>Form Id</Th>
-                        <Th>Status</Th>
-                        <Th>Creation Date</Th>
-                        <Th>Responses</Th>
-                    </Tr>
-                    </Thead>
-                    <Tbody bg={"pink"}>
-                    <Tr justifyContent="space-evenly" alignItems={"flex-start"} onClick={()=>console.log("aaaa")}>
-                        <Td>Survey Form</Td>
-                        <Td>xxx-xxx-xxx</Td>
-                        <Td>4 days left</Td>
-                        <Td>22/24/32</Td>
-                        <Td>0 Responses</Td>
-                    </Tr>
-                    <Tr justifyContent="space-evenly" alignItems={"flex-start"}>
-                        <Td>Survey Form</Td>
-                        <Td>xxx-xxx-xxx</Td>
-                        <Td>4 days left</Td>
-                        <Td>22/24/32</Td>
-                        <Td>0 Responses</Td>
-                    </Tr>
-                    <Tr justifyContent="space-evenly" alignItems={"flex-start"}>
-                        <Td>Survey Form</Td>
-                        <Td>xxx-xxx-xxx</Td>
-                        <Td>4 days left</Td>
-                        <Td>22/24/32</Td>
-                        <Td>0 Responses</Td>
-                    </Tr>
-                    </Tbody>
-                </Table>
-            </TableContainer>
+import {Box, Tag} from "@chakra-ui/react"
+import {useMoralis} from "react-moralis"
+import {Table, Badge, Typography, Button} from "web3uikit"
+import * as moment from "moment"
+const Dashboard = (props) => {
+    const {Moralis, account} = useMoralis();
+    const [formList, setFormList] = useState([])
+    useEffect(()=>{
+        async function fetchMyForms() {
+            const formList = await Moralis.Cloud.run("getFormsByUserId", {id: account});
+            setFormList(formList);
+            // await Moralis.Cloud.run("list_name", {"attributes"})
+        }
+        fetchMyForms()
+    },[account])
+    const closeModal = () => {
+        props.modaltoggle(false)
+    }
+    const openResponses = (formId) => {
+        console.log(formId)
+    }
+    return (
+        <Box>
+            <Box h="600px" w="100%" display="flex" justifyContent="center" alignItems={"center"}>
+                <Table
+                alignCellItems="center"
+                columnsConfig="1fr 2fr 1fr 1.5fr 1fr 1fr 0.7fr"
+                data={
+                    formList.map((formData) => {
+                        const dateLeft = moment("11/08/2022, 06:24:00", "DD/MM/YYYY hh:mm:ss").fromNow(); 
+                        return(
+                            <><div style={{marginLeft: '16px'}}><Typography color="#041836" variant="body16">{formData.name}</Typography></div></>,
+                            <Typography color="#68738D" variant="body16">{formData.formId}</Typography>,
+                            !formData.formId ? <Badge state="danger" text="Closed" /> : <Badge state="success" text="Active" />,
+                            <Typography color="#68738D" variant="body16">{formData.closingDate}</Typography>,
+                            <Tag size={'lg'} variant='solid' colorScheme='purple'>{formData.formType}</Tag>,
+                            <Tag size={'lg'} variant='solid' colorScheme='teal'>{dateLeft}</Tag>,
+                            <><Button color="blue" isTransparent size="small" text="Responses" onClick={()=>openResponses(formData.formId)}/></>
+                        )
+                    })
+                }
+                header={[
+                    <span>Name</span>,
+                    <span>Form Id</span>,
+                    <span>Status</span>,
+                    <span>Creation Date</span>,
+                    <span>Form Type</span>,
+                    <span>Day Left</span>,
+                    <span>Action</span>
+                ]}
+                isColumnSortable={[
+                    true,
+                    true,
+                    true,
+                    true,
+                    true,
+                    true
+                ]}
+                maxPages={3}
+                onPageNumberChanged={function noRefCheck(){}}
+                onRowClick={function noRefCheck(){}}
+                pageSize={5}
+                />
+            </Box>
         </Box>
-    </Box>
-  )
+    )
 }
 
 export default Dashboard;
