a79d6f546700f27349d9f41a0500dc52f17b8eef
ateet <ateet@propelld.com>
2022-08-05 02:28:20 +0530

    truncated

--- src/addressTruncate.js
+++ src/addressTruncate.js
@@ -1,5 +1,5 @@
 export function TruncateAddress(addr) {
     let first = addr.substr(0,10);
     let second = addr.substr(addr.length - 6);
-    return `${first}....${second}`
+    return `${first}....${second}`.toLowerCase()
 }
