9f48015b62094ce542986b0122e9c2bf358e1071
ateet <ateet@propelld.com>
2022-08-06 02:20:47 +0530

    collection form done

--- src/pages/CollectionForm.js
+++ src/pages/CollectionForm.js
@@ -1,136 +1,312 @@
-import React, { useState } from 'react'
-import {Box, Flex} from '@chakra-ui/react';
-import FormUtil from "./FormUtil"
-import FormCommon from "./FormCommon"
-import FormSubmit from "./FormSubmit"
+import React, { useState } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import FormUtil from "./FormUtil";
+import FormCommon from "./FormCommon";
+import FormSubmit from "./FormSubmit";
 import {
-    FormControl,
-    FormLabel,
-    FormHelperText,
-    Textarea,
-    Stack,
-    Radio,
-    Select,
-    Input
-  } from '@chakra-ui/react'
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Textarea,
+  Stack,
+  Radio,
+  Select,
+  Input,
+} from "@chakra-ui/react";
 import {
-    Alert,
-    AlertIcon,
-    AlertTitle,
-    AlertDescription,
-    Divider
-  } from '@chakra-ui/react'
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Divider,
+} from "@chakra-ui/react";
 import CollectionFormSubmitModal from "./CollectionFormSubmitModal";
+import { useMoralis } from "react-moralis";
+import { useForm } from "react-hook-form";
+import { getNanoid } from "../getNanoid";
+import { useNotification } from "web3uikit";
+import Loader from "./Loader";
+import * as moment from "moment";
 const CollectionForm = () => {
-    const [openModal, toggleModalOpen]=useState(false)
-    const submitForm = () => {
-        toggleModalOpen(true)
-    }
+  const [submitModal, setSubmitModal] = useState(false);
+  const [value, setValue] = useState(null);
+  const { Moralis, account } = useMoralis();
+  const [loading, setLoading] = useState(false);
+  const dispatch = useNotification();
+  let navigate = useNavigate();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const onSubmit = async (value) => {
+    setSubmitModal(true);
+    setValue(value);
+    console.log("event", value);
+  };
+
+  const handleNewNotification = (type, message, title) => {
+    dispatch({
+      type,
+      message: message,
+      title: title,
+      position: "topR",
+    });
+  };
+
+  const submitForms = async (data) => {
+    setSubmitModal(false);
+    console.log("data", data.walletAddress);
+    const formId = getNanoid();
+    const formCreationData = {
+      ethAddress: account,
+      formId: formId,
+      name: value.formName,
+      formType: "Collection",
+      status: true,
+      closingDate: moment(value.endDate).format("DD/MM/YYYY hh:mm:ss"),
+      description: value.description,
+    };
+    setLoading(true);
+    await Moralis.Cloud.run("addUpdateForms", formCreationData)
+      .then(async (result) => {
+        const dataSent = {
+          formId: formId,
+          walletAddress: data.walletAddress,
+          status: true
+        };
+        await Moralis.Cloud.run("addCollectionFormData", dataSent)
+          .then((result) => {
+            console.log("addSurveyFormData", result);
+            setLoading(false);
+            handleNewNotification("success", "Form Created", formId);
+            navigate("/");
+          })
+          .catch((ex) => {
+            console.log("addSurveyFormData", ex);
+            setLoading(false);
+            handleNewNotification("error", "Failed", ex);
+          });
+      })
+      .catch((ex) => {
+        console.log("addUpdateFormsError", ex);
+        setLoading(false);
+        handleNewNotification("error", "Failed", ex);
+      });
+  };
+
+  const closeModal = () => {
+    setSubmitModal(false);
+  };
   return (
-    <Flex width="full" align="center" justifyContent="center" direction={"column"} color="#fff">
-        <Box color="teal" fontSize="2xl" fontWeight="bold" letterSpacing="wider">
-            Collection Template
-        </Box>
-        <form>
-        <FormSubmit onSubmit={()=>submitForm()}/>
-        <FormUtil/>
-        <Box boxShadow="dark-lg"
+    <Flex
+      width="full"
+      align="center"
+      justifyContent="center"
+      direction={"column"}
+      color="#fff"
+    >
+      <Box color="teal" fontSize="2xl" fontWeight="bold" letterSpacing="wider">
+        Collection Template
+      </Box>
+      <Loader isLoading={loading} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormSubmit submitting={isSubmitting} />
+        {/* <FormUtil/> */}
+        <Box
+          boxShadow="dark-lg"
           p={8}
           my={4}
           borderRadius="13px"
           width="2xl"
-          color="#fff">
-                <FormControl>
-                    <FormLabel>Description</FormLabel>
-                    <Textarea size="md"/>
-                    <FormHelperText>Writing anything that u want to convey</FormHelperText>
+          color="#fff"
+        >
+          <Box my={4} textAlign="left">
+            <Box
+              display={"flex"}
+              flexDirection="row"
+              width="full"
+              justifyContent="space-between"
+            >
+              <Box width="270px">
+                <FormControl isRequired>
+                  <FormLabel>Form Name</FormLabel>
+                  <Input
+                    {...register("formName")}
+                    name="formName"
+                    type="text"
+                    required
+                  />
+                  <FormHelperText>Please Make Sure Its Unique</FormHelperText>
                 </FormControl>
-        </Box>
-            <Box boxShadow="dark-lg"
-          p={8}
-          my={4}
-          borderRadius="13px"
-          width="2xl"
-          color="#fff">
-                <Box display={"flex"} flexDirection="row" width="full" justifyContent="space-between" height="full">
-                    <Box width="270px">
-                        <FormControl>
-                            <FormLabel>Your Company Name</FormLabel>
-                            <Input type="text"/>
-                        </FormControl>
-                    </Box>
-                    <Box width="270px">
-                        <FormControl>
-                            <FormLabel>Your Twitter</FormLabel>
-                            <Input type="text"/>
-                        </FormControl>
-                    </Box>
-                </Box>
-                <Box display={"flex"} flexDirection="row" width="full" justifyContent="space-between" height="full">
-                <Box width="270px">
-                    <FormControl>
-                        <FormLabel>Your Website</FormLabel>
-                        <Input type="text"/>
-                    </FormControl>
-                </Box>
-                <Box width="270px">
-                    <FormControl>
-                        <FormLabel>Your Discord</FormLabel>
-                        <Input type="text"/>
-                    </FormControl>
-                </Box>
+              </Box>
+              <Box width="270px">
+                <FormControl isRequired>
+                  <FormLabel>End Date</FormLabel>
+                  <Input
+                    {...register("endDate")}
+                    name="endDate"
+                    type="datetime-local"
+                    value={moment().format("DD/MM/YYYY HH:MM a")}
+                    required
+                  />
+                  <FormHelperText>
+                    After this Date, your form will be disabled for inputs{" "}
+                  </FormHelperText>
+                </FormControl>
+              </Box>
             </Box>
-            <Box mt={4}>
-                <Alert boxShadow="dark-lg"
+          </Box>
+          <Box>
+            <Alert
+              boxShadow="dark-lg"
               status="error"
               borderRadius={13}
               color="gray"
-              fontWeight="medium">
-                    <AlertIcon />
-                    <AlertDescription>Disclaimer — This will be shown with the total amount you collected.</AlertDescription>
-                </Alert>
-            </Box>
-            </Box>
-            <Divider orientation='horizontal' my={7}/>
-            <Box>
-                <Alert status="info"
-            borderRadius={13}
-            boxShadow="dark-lg"
-            color="gray"
-            fontWeight="medium">
-                    <AlertIcon />
-                    <AlertDescription>Everything below this will be filled by your users</AlertDescription>
-                </Alert>
-            </Box>
-            <Box my={4}>
-                <FormCommon/>
-            </Box>
-            <Box  boxShadow="dark-lg"
+              fontWeight="medium"
+            >
+              <AlertIcon />
+              <AlertDescription>
+                Disclaimer — Please note, once submitted you can't change this.
+              </AlertDescription>
+            </Alert>
+          </Box>
+        </Box>
+        <Box
+          boxShadow="dark-lg"
           p={8}
           my={4}
           borderRadius="13px"
           width="2xl"
-          color="#fff">
-                <FormControl mt={1} isDisabled>
-                    <FormLabel>Address you want your Merch to get Delivered</FormLabel>
-                    <Textarea size="md"/>
-                </FormControl>
-                <FormControl mt={5} isDisabled>
-                    <FormLabel>What's your t-shirt Size</FormLabel>
-                    <Stack direction='row'>
-                        <Radio value='1'>s</Radio>
-                        <Radio value='2'>m</Radio>
-                        <Radio value='3'>l</Radio>
-                        <Radio value='4'>xl</Radio>
-                        <Radio value='5'>xxl</Radio>
-                        <Radio value='5'>xxxl</Radio>
-                    </Stack>
-                </FormControl>
+          color="#fff"
+        >
+          <FormControl isRequired>
+            <FormLabel>Description</FormLabel>
+            <Textarea {...register("description")} size="md" />
+            <FormHelperText>
+              Writing anything that u want to convey
+            </FormHelperText>
+          </FormControl>
+        </Box>
+        <Box
+          boxShadow="dark-lg"
+          p={8}
+          my={4}
+          borderRadius="13px"
+          width="2xl"
+          color="#fff"
+        >
+          <Box
+            display={"flex"}
+            flexDirection="row"
+            width="full"
+            justifyContent="space-between"
+            height="full"
+          >
+            <Box width="270px">
+              <FormControl isRequired>
+                <FormLabel>Your Company Name</FormLabel>
+                <Input
+                  {...register("companyName")}
+                  name="companyName"
+                  type="text"
+                />
+              </FormControl>
             </Box>
-        </form>
-        <CollectionFormSubmitModal openModal={openModal} onClose={()=>toggleModalOpen(false)}/>
+            <Box width="270px">
+              <FormControl isRequired>
+                <FormLabel>Your Twitter</FormLabel>
+                <Input {...register("twitter")} name="twitter" type="text" />
+              </FormControl>
+            </Box>
+          </Box>
+          <Box
+            display={"flex"}
+            flexDirection="row"
+            width="full"
+            justifyContent="space-between"
+            height="full"
+          >
+            <Box width="270px">
+              <FormControl isRequired>
+                <FormLabel>Your Website</FormLabel>
+                <Input {...register("website")} name="website" type="text" />
+              </FormControl>
+            </Box>
+            <Box width="270px">
+              <FormControl isRequired>
+                <FormLabel>Your Discord</FormLabel>
+                <Input {...register("discord")} name="discord" type="text" />
+              </FormControl>
+            </Box>
+          </Box>
+          <Box mt={4}>
+            <Alert
+              boxShadow="dark-lg"
+              status="error"
+              borderRadius={13}
+              color="gray"
+              fontWeight="medium"
+            >
+              <AlertIcon />
+              <AlertDescription>
+                Disclaimer — This will be shown with the total amount you
+                collected.
+              </AlertDescription>
+            </Alert>
+          </Box>
+        </Box>
+        <Divider orientation="horizontal" my={7} />
+        <Box>
+          <Alert
+            status="info"
+            borderRadius={13}
+            boxShadow="dark-lg"
+            color="gray"
+            fontWeight="medium"
+          >
+            <AlertIcon />
+            <AlertDescription>
+              Everything below this will be filled by your users
+            </AlertDescription>
+          </Alert>
+        </Box>
+        <Box my={4}>
+          <FormCommon />
+        </Box>
+        <Box
+          boxShadow="dark-lg"
+          p={8}
+          my={4}
+          borderRadius="13px"
+          width="2xl"
+          color="#fff"
+        >
+          <FormControl mt={1} isDisabled>
+            <FormLabel>Address you want your Merch to get Delivered</FormLabel>
+            <Textarea size="md" />
+          </FormControl>
+          <FormControl mt={5} isDisabled>
+            <FormLabel>What's your t-shirt Size</FormLabel>
+            <Stack direction="row">
+              <Radio value="1">s</Radio>
+              <Radio value="2">m</Radio>
+              <Radio value="3">l</Radio>
+              <Radio value="4">xl</Radio>
+              <Radio value="5">xxl</Radio>
+              <Radio value="5">xxxl</Radio>
+            </Stack>
+          </FormControl>
+        </Box>
+      </form>
+      <CollectionFormSubmitModal
+        onClose={closeModal}
+        submitForm={submitModal}
+        onSubmit={submitForms}
+      />
     </Flex>
-  )
-}
+  );
+};
 
-export default CollectionForm
+export default CollectionForm;
--- src/pages/CollectionFormSubmitModal.js
+++ src/pages/CollectionFormSubmitModal.js
@@ -6,13 +6,25 @@ import {Button, Modal,
     ModalFooter,
     ModalBody,
     ModalCloseButton, FormControl, Input,FormLabel, useDisclosure, FormErrorMessage} from "@chakra-ui/react"
+
+import {useForm} from "react-hook-form"
+import { useMoralis } from "react-moralis";
 const CollectionFormSubmitModal = (props) => {
-  
+    const {
+      handleSubmit,
+      register,
+      formState: { errors, isSubmitting },
+    } = useForm()
+    const {Moralis, account} = useMoralis();
+    const onSubmitModal = async(value) => {
+      console.log(value);
+      props.onSubmit(value)
+    }
     return (
       <>
         <Modal
           onClose={props.onClose}
-          isOpen={props.openModal}
+          isOpen={props.submitForm}
           motionPreset="slideInBottom"
           size="2xl"
         >
@@ -20,20 +32,23 @@ const CollectionFormSubmitModal = (props) => {
           <ModalContent>
             <ModalHeader>Please Confirm the wallet Address in which you will receive money</ModalHeader>
             <ModalCloseButton />
+            <form onSubmit={handleSubmit(onSubmitModal)}>
             <ModalBody pb={10}>
-              <FormControl isRequired>
-                <FormLabel>Amount to pool for survey</FormLabel>
-                <Input />
-                <FormErrorMessage>Can't submit without funds.</FormErrorMessage>
-              </FormControl>
+              
+                <FormControl isRequired>
+                  <FormLabel>Wallet Address</FormLabel>
+                  <Input {...register("walletAddress")} name="walletAddress" value={account}/>
+                  <FormErrorMessage>Can't submit without funds.</FormErrorMessage>
+                </FormControl>
             </ModalBody>
   
             <ModalFooter>
-              <Button colorScheme='blue' mr={3}>
+              <Button colorScheme='blue' mr={3} type="submit">
                 Submit
               </Button>
               <Button onClick={props.onClose}>Go Back</Button>
             </ModalFooter>
+            </form>
           </ModalContent>
         </Modal>
       </>
