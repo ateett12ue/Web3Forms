c2002a326bcba50fa3c46be45a8663d2256e7c9c
ateet <ateet@propelld.com>
2022-08-07 12:23:04 +0530

    hardhat config added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,36 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+ const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY, MUMBAI_API_URL, POLYGONSCAN_API_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: MUMBAI_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY,
+  },
 };
